be6610474a673feb824b09226db2b4c1ea34b007
lolasanchezz <lolansanchez@icloud.com>
2024-09-22 15:58:48 -0400

    logging in works

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,41 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+require('dotenv').config()
 const email = process.env.email;
 const password = process.env.password;
+
 const test = async() => {
+
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
-    await page.goto(`https://app.blackbaud.com/signin/?login_hint=${email}`);
-    await page.locator('#identifierNext > div > button > span').click();
-   await page.locator('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input').fill(password)
+    //signing in
+    await page.goto(`https://app.blackbaud.com/signin/?`);
+    await page.waitForSelector('[icon = "google"]')
+    await page.click('[icon = "google"]')
+    await page.waitForSelector('[type = "email"]')
+    
+    
+    
+    await page.type('[type = "email"]', email)
+    await page.click('[id = "identifierNext"]')
+   await page.waitForSelector('[type = "password"]', {visible: true})
+   await page.type('[type="password"]', password);
+   await page.waitForSelector('#passwordNext');
+   await page.click("#passwordNext");
+   await page.waitForNavigation();
+   await page.waitForSelector('[eventbuttonname = "Portal link"]')
+   await page.click('[eventbuttonname="Portal link"]')
+   await page.waitForNavigation();
+   console.log("logged in!!")
 
-   
+
+   //now trying to take all the text from each assignment
+    
+
+
+
+
+
+
     
 }
 test();
